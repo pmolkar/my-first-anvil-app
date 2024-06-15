3de1cd9506fa86cbaa4354c316502bbe9fc6a3d0
pmolkar@gmail.com <pmolkar@gmail.com>
2024-06-15 04:38:15 +0000

    Created LICENSE.txt; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,3 +12,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.repeating_panel_1.items = app_tables.test.search()
+
+  def data_grid_1_show(self, **event_args):
+    """This method is called when the data grid is shown on the screen"""
+    pass
